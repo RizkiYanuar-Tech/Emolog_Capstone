5640d1c7a0b39fe4b963ce348a577033c69a82c1
Rizki Yanuar <ryanuar83@gmail.com>
2025-06-21 01:49:34 +0700

    update

--- Emolog-Frontend/webpack.common.js
+++ Emolog-Frontend/webpack.common.js
@@ -41,7 +41,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './pages/index.html',
-      filename: 'pages/index.html'
+      filename: 'index.html'
     }),
     new HtmlWebpackPlugin({
       template: './pages/register/register.html',
